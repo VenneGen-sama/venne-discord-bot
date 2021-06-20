ff225fb374fd86571f46ebe4d0af517b1fa46dbd
VenneGen-sama <itsvinc97@gmail.com>
2021-06-20 18:40:17 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+exports.Token = `ODUzMzE3MDg5NDg5MTkwOTEz.YMTndw.TN8pVRZPlGMdNfv7RjGzcyvkVZ4`; //Bot Token - Important
+exports.Default_Prefix = `f.`; //Bot Default Prefix - Important
+exports.Color = `RANDOM`; //Bot All Embeds Color - Use CAPS For Name - Important
+exports.WelcomeImage = `https://cdn.discordapp.com/attachments/855737554993283112/856049775605186560/20210620_135602.jpg`; //Welcome Image Link - Default Image By Me (If No Link Provided)
+exports.LeaveImage = `https://cdn.discordapp.com/attachments/855737554993283112/856052272496508948/wallpapertip_night-sky-wallpaper_15885.jpg`; //Leave Image Link - Default Image By Me (If No Link Provided)
+exports.Support = `https://discord.gg/GJzF72bFtD`; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
+//Make Sure To On Intents!
